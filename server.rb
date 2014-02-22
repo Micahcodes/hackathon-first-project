2d16271de38d6b88ce1f550cbe7dad0c9c3357c7
Kathryn Beaty <kabeaty@gmail.com>
2014-02-21 22:20:42 -0600

    Add get requests for pages

--- server.rb
+++ server.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 require 'rubygems'
 
 get '/home' do
-  "Hey, this is SplashCard Homepage"
   erb :homepage
+end
+
+get '/create' do
+  erb :create
+end
+
+get '/create/2' do
+  erb :create_2
+end
+
+get '/create/3' do
+  erb :create_3
+end
+
+get '/create/4' do
+  erb :create_final
 end
