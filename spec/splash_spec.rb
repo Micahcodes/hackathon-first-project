a72fdddb15f2963e9292e4d79367c7246756d6c3
Kathryn Beaty <kabeaty@gmail.com>
2014-02-22 14:41:39 -0600

    Add test outlines

--- spec/splash_spec.rb
+++ spec/splash_spec.rb
@@ -1,3 +1,21 @@
 require 'rubygems'
 require 'rspec'
+require 'vcr'
+require 'pry'
 require_relative '..lib/splash.rb'
+
+# describe InstaFeed::PictureInfo do
+#   describe '.get_picture_info' do
+
+#   it ""
+
+#   end
+# end
+
+# describe TwitterFeed::TweetInfo do
+#   describe ".get_tweet_info" do
+
+#     it ""
+
+#   end
+# end
