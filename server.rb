3e80bfa92f4b49464a567406f92e63800c806c75
Micah Stewart <stewart.micah@gmail.com>
2014-02-23 15:06:10 -0600

    fix paths of create images

--- server.rb
+++ server.rb
@@ -57,10 +57,7 @@ end
 post '/create/2' do
   @recipient = params[:recipient]
   @message = params[:message]
-  erb :create_3
-end
 
-get '/create/3/:pic' do
   @pic = params[:pic]
   @user_pics = session[:user_pics]
 
@@ -72,6 +69,11 @@ get '/create/3/:pic' do
   erb :create_3
 end
 
+get '/create/3/:pic' do
+
+  erb :create_3
+end
+
 get '/create/4' do
   erb :create_final
 end
@@ -79,43 +81,56 @@ end
 
 
 ###### Twitter ######
+configure do
+  enable :sessions
+end
+helpers do
+  def admin?
+    true
+  end
+end
 
-# get '/private' do
-#   halt(401,'Not Authorized') unless admin?
-#   "THis is the private page - members only"
-# end
 
-# get '/auth/twitter/callback' do
+get '/private' do
+  halt(401,'Not Authorized') unless admin?
+  "THis is the private page - members only"
+end
 
-#   load 'lib/credentials.rb'
-#   session[:admin] = true
-#   session[:username] = env['omniauth.auth']['info']['name']
-#   "<h1>Hi #{session[:username]}!</h1>"
-#   puts env['omniauth.auth']
-#   @twitter_handle = env['omniauth.auth']['info']['nickname']
-#   require 'twitter'
-#   client = Twitter::REST::Client.new do |config|
-#   config.consumer_key = $twitter_consumer_key
-#   config.consumer_secret = $twitter_consumer_secret
-#   config.access_token = $twitter_access_token
-#   config.access_token_secret = $twitter_access_secret
-#   end
-#   #puts client.user_timeline(@twitter_handle)
-#   @list = client.user_timeline(@twitter_handle)
-#   @tweet_array = []
-#   @list.each do |tweet|
-#     if tweet[:full_text].match(/\A[^@]/)
-#       @tweet_array.push(tweet[:full_text])
-#     end
-#   end
-#   erb :create_2
-# end
+get '/login' do
+  redirect to("/auth/twitter")
+end
 
-# use OmniAuth::Builder do
-#   load 'lib/credentials.rb'
-#   provider :twitter, $twitter_consumer_key, $twitter_consumer_secret
-# end
+get '/auth/twitter/callback' do
 
-# get '/tweet' do
-#   'https://upload.twitter.com/1/statuses/update_with_media.json'
-# end
+  load 'lib/credentials.rb'
+  session[:admin] = true
+  session[:username] = env['omniauth.auth']['info']['name']
+  "<h1>Hi #{session[:username]}!</h1>"
+  puts env['omniauth.auth']
+  @twitter_handle = env['omniauth.auth']['info']['nickname']
+  require 'twitter'
+  client = Twitter::REST::Client.new do |config|
+  config.consumer_key = $twitter_consumer_key
+  config.consumer_secret = $twitter_consumer_secret
+  config.access_token = $twitter_access_token
+  config.access_token_secret = $twitter_access_secret
+  end
+  #puts client.user_timeline(@twitter_handle)
+  @list = client.user_timeline(@twitter_handle)
+  @tweet_array = []
+  @list.each do |tweet|
+    if tweet[:full_text].match(/\A[^@]/)
+      @tweet_array.push(tweet[:full_text])
+    end
+  end
+  erb :create_2
+end
+
+use OmniAuth::Builder do
+  load 'lib/credentials.rb'
+  provider :twitter, $twitter_consumer_key, $twitter_consumer_secret
+end
+
+get '/tweet' do
+  'https://upload.twitter.com/1/statuses/update_with_media.json'
+end
